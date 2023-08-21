9a3820768aa196e49551973cc98b847e54afce5a
jack <ysj@rmsoft.kr>
2023-08-21 16:36:45 +0900

    2023.08.21 일일 업무

---
+++ cycle0/mt_example/setup_environment.sh
@@ -0,0 +1,20 @@
+# 1. install gcc and cmake
+sudo apt install -y gcc
+sudo apt install -y cmake
+
+# 2. install cuda
+wget wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt-get update 
+sudo apt-get install -y cuda
+
+# 3. install pip
+sudo apt install python3-pip 
+
+# 4. install python libraries 
+pip install transformers datasets torch torchaudio torchvision numpy soundfile librosa 
+sudo apt-get install sox 
+pip install sox 
+sudo apt update 
+sudo apt install ffmpeg 
+pip install -U accelerate 
