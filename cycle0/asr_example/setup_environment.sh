b9af59a9fdf49752ec501932da0acce76fe3180a
jack <ysj@rmsoft.kr>
2023-08-21 18:41:54 +0900

    2023.08.21 일일 업무

--- cycle0/asr_example/setup_environment.sh
+++ cycle0/asr_example/setup_environment.sh
@@ -2,14 +2,14 @@
 sudo apt install -y gcc
 sudo apt install -y cmake
 
-# 2. install cuda
+2. install cuda
 wget wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.1-1_all.deb
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt-get update 
 sudo apt-get install -y cuda
 
-# 3. install pip
-sudo apt install python3-pip 
+3. install pip
+sudo apt install -y python3-pip 
 
 # 4. install mecab
 wget https://bitbucket.org/eunjeon/mecab-ko/downloads/mecab-0.996-ko-0.9.2.tar.gz
@@ -18,16 +18,15 @@ cd mecab-0.996-ko-0.9.2
 ./configure
 make
 make check
-make install
+sudo make install
 sudo ldconfig
-mecab --version
 pip install mecab-python3 
 pip install unidic-lite # dependecy for mecab tagging
 
 # 5. install python libraries 
 pip install pykakasi transformers datasets torch torchaudio torchvision numpy jiwer soundfile librosa 
-sudo apt-get install sox 
+sudo apt-get install -y sox 
 pip install sox 
 sudo apt update 
-sudo apt install ffmpeg 
+sudo apt install -y ffmpeg 
 pip install -U accelerate 
