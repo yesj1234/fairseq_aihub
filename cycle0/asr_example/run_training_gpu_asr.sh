b358a002cd9dcf4987a83c4f8486efa1bc6ff4d4
jack <juesc007@gmail.com>
2023-08-23 15:42:51 +0900

    2023.08.23 일일 업무

--- cycle0/asr_example/run_training_gpu_asr.sh
+++ cycle0/asr_example/run_training_gpu_asr.sh
@@ -3,7 +3,7 @@
 LOCAL_RANK=0,1,2,3 \
 CUDA_VISIBLE_DEVICES=0,1,2,3 \
 python3 -m torch.distributed.launch --nproc_per_node 4 \
---use-env run_training.py \
-run_training_gpu_asr.json \
+--use-env run_speech_recognition_ctc.py \
+run_speech_recognition_ctc.json \
 --chars_to_ignore [\,\?\.\!\-\;\:\"\“\‘\”\ ‘、。．！，・―─~｢｣『』〆｡\\\\※\[\]\{\}「」〇？…]
 
