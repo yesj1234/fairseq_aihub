40966d64be36c150c1fd5055d5ef1855b8ef76fc
jack <juesc007@gmail.com>
2023-08-29 08:45:13 +0000

    2023.08.29 코드 수정

--- cycle0/1.prepare_from_json.py
+++ cycle0/1.prepare_from_json.py
@@ -24,10 +24,10 @@ def main(args):
     os.makedirs(os.path.join(args.asr_dest_file, "asr_split"), exist_ok=True)
     os.makedirs(os.path.join(args.mt_dest_file, "mt_split"), exist_ok=True)
 
+    sound_file_paths, sound_file_transcriptions, sound_file_translations = [], [], []
     for root, dir, files in os.walk(args.jsons):
         if files:
             print(f"json files from {os.path.join(root)}")
-            sound_file_paths, sound_file_transcriptions, sound_file_translations = [], [], []
             for file in files:
                 _, ext = os.path.splitext(file)
                 if ext == ".json":
@@ -37,41 +37,41 @@ def main(args):
                         sound_file_transcriptions.append(transcription)
                         sound_file_translations.append(translation)
                         
-            sound_file_path_train, sound_file_path_validate, sound_file_path_test = np.split(sound_file_paths, [int(len(files)*0.8), int(len(files)*0.9)])
-            transcription_train, transcription_validate, transcription_test = np.split(sound_file_transcriptions, [int(len(files)*0.8), int(len(files)*0.9)])
-            translation_train, translation_validate, translation_test = np.split(sound_file_translations, [int(len(files)*0.8), int(len(files)*0.9)])
-                            
-            assert len(sound_file_path_train) == len(transcription_train),  "train split 길이 안맞음."
-            assert len(sound_file_path_train) == len(translation_train), "train split 길이 안맞음."
-            assert len(transcription_train) == len(translation_train), "train split 길이 안맞음."
-            
-            assert len(sound_file_path_test) == len(transcription_test),  "test split 길이 안맞음."
-            assert len(sound_file_path_test) == len(translation_test), "test split 길이 안맞음."
-            assert len(transcription_test) == len(translation_test), "test split 길이 안맞음."
-            
-            assert len(sound_file_path_validate) == len(transcription_validate),  "validate split 길이 안맞음."
-            assert len(sound_file_path_validate) == len(translation_validate), "validate split 길이 안맞음."
-            assert len(transcription_validate) == len(translation_validate), "validate split 길이 안맞음."
-                            
-            with open(f"{os.path.join(args.asr_dest_file, 'asr_split','asr_train.tsv')}", "a+") as asr_train, \
-                open(f"{os.path.join(args.asr_dest_file, 'asr_split','asr_test.tsv')}", "a+") as asr_test, \
-                open(f"{os.path.join(args.asr_dest_file, 'asr_split','asr_validation.tsv')}", "a+") as asr_validate:
-                for i in range(len(sound_file_path_train)-1):
-                    asr_train.write(f"{sound_file_path_train[i]} :: {transcription_train[i]}\n")
-                for i in range(len(sound_file_path_test)-1):
-                    asr_test.write(f"{sound_file_path_test[i]} :: {transcription_test[i]}\n")
-                for i in range(len(sound_file_path_validate)-1):    
-                    asr_validate.write(f"{sound_file_path_validate[i]} :: {transcription_validate[i]}\n")
+    sound_file_path_train, sound_file_path_validate, sound_file_path_test = np.split(sound_file_paths, [int(len(sound_file_paths)*0.8), int(len(sound_file_paths)*0.9)])
+    transcription_train, transcription_validate, transcription_test = np.split(sound_file_transcriptions, [int(len(sound_file_transcriptions)*0.8), int(len(sound_file_transcriptions)*0.9)])
+    translation_train, translation_validate, translation_test = np.split(sound_file_translations, [int(len(sound_file_translations)*0.8), int(len(sound_file_translations)*0.9)])
                     
-            with open(f"{os.path.join(args.mt_dest_file, 'mt_split','mt_train.tsv')}", "a+") as mt_train, \
-                open(f"{os.path.join(args.mt_dest_file, 'mt_split','mt_test.tsv')}", "a+") as mt_test, \
-                open(f"{os.path.join(args.mt_dest_file, 'mt_split','mt_validation.tsv')}", "a+") as mt_validate:
-                for i in range(len(sound_file_path_train)-1):
-                    mt_train.write(f"{transcription_train[i]} :: {translation_train[i]}\n")
-                for i in range(len(sound_file_path_test)-1):    
-                    mt_test.write(f"{transcription_test[i]} :: {translation_test[i]}\n")
-                for i in range(len(sound_file_path_validate)-1):
-                    mt_validate.write(f"{transcription_validate[i]} :: {translation_validate[i]}\n")
+    assert len(sound_file_path_train) == len(transcription_train),  "train split 길이 안맞음."
+    assert len(sound_file_path_train) == len(translation_train), "train split 길이 안맞음."
+    assert len(transcription_train) == len(translation_train), "train split 길이 안맞음."
+    
+    assert len(sound_file_path_test) == len(transcription_test),  "test split 길이 안맞음."
+    assert len(sound_file_path_test) == len(translation_test), "test split 길이 안맞음."
+    assert len(transcription_test) == len(translation_test), "test split 길이 안맞음."
+    
+    assert len(sound_file_path_validate) == len(transcription_validate),  "validate split 길이 안맞음."
+    assert len(sound_file_path_validate) == len(translation_validate), "validate split 길이 안맞음."
+    assert len(transcription_validate) == len(translation_validate), "validate split 길이 안맞음."
+                    
+    with open(f"{os.path.join(args.asr_dest_file, 'asr_split','asr_train.tsv')}", "a+") as asr_train, \
+        open(f"{os.path.join(args.asr_dest_file, 'asr_split','asr_test.tsv')}", "a+") as asr_test, \
+        open(f"{os.path.join(args.asr_dest_file, 'asr_split','asr_validation.tsv')}", "a+") as asr_validate:
+        for i in range(len(sound_file_path_train)-1):
+            asr_train.write(f"{sound_file_path_train[i]} :: {transcription_train[i]}\n")
+        for i in range(len(sound_file_path_test)-1):
+            asr_test.write(f"{sound_file_path_test[i]} :: {transcription_test[i]}\n")
+        for i in range(len(sound_file_path_validate)-1):    
+            asr_validate.write(f"{sound_file_path_validate[i]} :: {transcription_validate[i]}\n")
+            
+    with open(f"{os.path.join(args.mt_dest_file, 'mt_split','mt_train.tsv')}", "a+") as mt_train, \
+        open(f"{os.path.join(args.mt_dest_file, 'mt_split','mt_test.tsv')}", "a+") as mt_test, \
+        open(f"{os.path.join(args.mt_dest_file, 'mt_split','mt_validation.tsv')}", "a+") as mt_validate:
+        for i in range(len(sound_file_path_train)-1):
+            mt_train.write(f"{transcription_train[i]} :: {translation_train[i]}\n")
+        for i in range(len(sound_file_path_test)-1):    
+            mt_test.write(f"{transcription_test[i]} :: {translation_test[i]}\n")
+        for i in range(len(sound_file_path_validate)-1):
+            mt_validate.write(f"{transcription_validate[i]} :: {translation_validate[i]}\n")
                                 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
