2d9b8305fdf5a0a6e7242395ff8f017f84a9e871
jack <juesc007@gmail.com>
2023-08-30 08:14:04 +0000

    2023.08.30 전사문 refining script 작성

--- utils/refine_data.py
+++ utils/refine_data.py
@@ -1,7 +1,8 @@
 import os 
 import re 
-pattern = re.compile("\([^\/]+\)\/\([^\/\(\)]+\)")
-bracket_pattern = re.compile("[\(\)]")
+bracket_pair_pattern = re.compile("\([^\/]+\)\/\([^\/\(\)]+\)") # (이거)/(요거) 모양 패턴
+bracket_pattern = re.compile("[\(\)]") # (문자) 모양 패턴
+bracket_ex_pattern = re.compile("\/\([^\/]+\)") # 뭣뭣/(무엇무엇) 모양 패턴
 for root, _dir, files in os.walk("./"):
     for file in files:
         fname, ext = os.path.splitext(file)
@@ -9,9 +10,10 @@ for root, _dir, files in os.walk("./"):
             with open(os.path.join(root, file), "r+", encoding="utf-8") as original_file, open(os.path.join(root, f"{fname}_refined.tsv"), "w+", encoding="utf-8") as refined_file:
                 lines = original_file.readlines()
                 new_lines = []
+                # ()/() 모양 패턴 제거 
                 for line in lines: 
                     _path, target_text = line.split(" :: ")
-                    matches = re.findall(pattern, target_text)
+                    matches = re.findall(bracket_pair_pattern, target_text)
                     if matches:
                         for item in matches:
                             first_part = item.split("/")[0]
@@ -20,9 +22,18 @@ for root, _dir, files in os.walk("./"):
                         new_lines.append(line)
                     else:
                         new_lines.append(line)
-                for new in new_lines:
-                    refined_file.write(f"{new}")
-                    
-                
+                # /() 모양 제거
+                for i, new_line in enumerate(new_lines):
+                    _path, target_text = new_line.split(" :: ")
+                    matches = re.findall(bracket_ex_pattern, target_text)
+                    if matches:
+                        for item in matches:
+                            first_part = item.split("/")[0]
+                            new_line = new_line.replace(item, first_part)
+                        new_lines[i] = new_line
+                    else:
+                        pass
+                for l in new_lines:
+                    refined_file.write(l)
                 
                         
