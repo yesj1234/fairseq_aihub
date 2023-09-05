7a048b16393419ef92465f6b521f932c18684bfe
jack <juesc007@gmail.com>
2023-09-05 23:06:20 +0000

    2023.09.06 일일 업무

---
+++ cycle0/asr_example/run_speech_recognition_seq2seq.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash 
+
+LOCAL_RANK=0,1,2,3 \
+CUDA_VISIBLE_DEVICES=0,1,2,3 \
+python3 -m torch.distributed.launch --nproc_per_node 4 \
+--use-env run_speech_recognition_ctc.py \
+run_speech_recognition_ctc.json \
